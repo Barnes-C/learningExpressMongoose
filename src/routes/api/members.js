2491a948dda4da3d6407df1f70dab24bab2ef2ad
Christopher Barnes <Christopher@Barnes.biz>
2020-09-24 23:23:48 +0200

    finished get, post & delete operations

--- src/routes/api/members.js
+++ src/routes/api/members.js
@@ -3,21 +3,48 @@ const { hashSync } = require('bcrypt');
 const mongoose = require('mongoose');
 const HttpStatus = require('../../middleware/httpStatus');
 const Member = require('../../member/entity/member.model');
-const members = require('../../middleware/config/db/Members');
 const logger = require('../../middleware/logger');
+const { update } = require('../../member/entity/member.model');
 
 const router = express.Router();
 
 const SALT_ROUNDS = 10;
 
 router
+
   // Get all Members
-  .get('/', (_, res) => res.json(members))
-  // Get all Members by Id
-  .get('/:id', (req, res) => {
-    // const { id } = req.params;
-    // Member.find();
+  .get('/', (req, res) => {
+    Member.find()
+      .exec()
+      .then((docs) => {
+        logger.info(docs);
+        res.status(HttpStatus.OK).json(docs);
+      })
+      .catch((err) => {
+        res.status(HttpStatus.INTERNAL_ERROR).json({ error: err });
+      });
   })
+
+  // Get Member by Id
+  .get('/:id', (req, res) => {
+    const { id } = req.params;
+    Member.findById(id)
+      .exec()
+      .then((doc) => {
+        if (doc) {
+          res.status(HttpStatus.OK).json(doc);
+        } else {
+          res
+            .status(HttpStatus.NOT_FOUND)
+            .json({ msg: 'No valid entry found for provided ID' });
+        }
+      })
+      .catch((err) => {
+        res.status(HttpStatus.INTERNAL_ERROR).json({ error: err });
+        logger.error(err);
+      });
+  })
+
   // Create Member
   .post('/', (req, res) => {
     const { firstName, lastName, password } = req.body;
@@ -28,70 +55,61 @@ router
         .send('<p>Please include a name, last name and password</p>');
     }
 
-    // Encrypting Password via bcrypt: $2b$[cost]$[22 character salt][31 character hash]
-    const pwd = hashSync(password, SALT_ROUNDS);
-
     const member = new Member({
       _id: new mongoose.Types.ObjectId(),
       firstName,
       lastName,
-      password: pwd,
+      password: hashSync(password, SALT_ROUNDS),
       subscribed: true,
       age: null,
       created: Date.now(),
     });
+
     member
       .save()
       .then((result) => {
-        res.redirect('/');
+        res.status(HttpStatus.CREATED).json(result);
         logger.info(result);
       })
       .catch((err) => {
-        res
-          .status(HttpStatus.BAD_REQUEST)
-          .send(`unable to save to database, ${err}`)
-          .redirect('/');
+        res.status(HttpStatus.BAD_REQUEST).json({ error: err });
         logger.error(err);
       });
-
-    members.push(member);
   })
+
   // Update Member by Id
   .put('/:id', (req, res) => {
-    const { id, name, password } = req.body;
-    // eslint-disable-next-line no-underscore-dangle
-    const found = members.some((member) => member._id === id);
-    if (found) {
-      members.forEach((member) => {
-        // eslint-disable-next-line no-underscore-dangle
-        if (member._id === id) {
-          member.name = name || member.name; // eslint-disable-line no-param-reassign
-          member.password = password || member.password; // eslint-disable-line no-param-reassign
-          res.json({ msg: 'Member updated', member });
-        }
-      });
-    } else {
-      res.status(HttpStatus.NOT_FOUND).json({
-        msg: `No member with the id of ${id}`,
-      });
+    const { id, newFirstName, newLastName, newPassword } = req.body;
+    const target = mongoose.Types.ObjectId(id);
+
+    const updateOps = {};
+    for (const ops of req.body) {
+      updateOps[ops.propName] = ops.value;
     }
+    Member.updateOne({ _id: target }, { $set: updateOps })
+      .exec()
+      .then((result) => {
+        logger.info(result);
+        res.status(HttpStatus.OK).json(result);
+      })
+      .catch((err) => {
+        logger.error(err);
+        res.status(HttpStatus.INTERNAL_ERROR).json({ error: err });
+      });
   })
+
   // Delete Member by Id
   .delete('/:id', (req, res) => {
-    const { id } = req.body;
-    // eslint-disable-next-line no-underscore-dangle
-    const found = members.some((member) => member._id === id);
-    if (found) {
-      res.json({
-        msg: 'Member deleted',
-        // eslint-disable-next-line no-underscore-dangle
-        members: members.filter((member) => member._id !== id),
+    const { id } = req.params;
+    const target = mongoose.Types.ObjectId(id);
+    Member.deleteOne({ _id: target })
+      .exec()
+      .then((result) => {
+        res.status(HttpStatus.OK).json(result);
+      })
+      .catch((err) => {
+        res.status(HttpStatus.INTERNAL_ERROR).json({ error: err });
       });
-    } else {
-      res.status(HttpStatus.NOT_FOUND).json({
-        msg: `No member with the id of ${id}`,
-      });
-    }
   });
 
 module.exports = router;

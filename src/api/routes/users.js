5684545fcfa760845d2e1c52e26a77af67f48c9b
Christopher Barnes <Christopher@Barnes.biz>
2020-09-30 18:51:46 +0200

    added login route

--- src/api/routes/users.js
+++ src/api/routes/users.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const HttpStatus = require('../../middleware/httpStatus');
 const User = require('../models/user');
 const logger = require('../../middleware/logger');
+const { Http } = require('winston/lib/winston/transports');
 
 const router = express.Router();
 
@@ -12,93 +13,6 @@ const port = process.env.PORT || 5000;
 const SALT_ROUNDS = 10;
 
 router
-
-  // Get all Users
-  .get('/', (_, res) => {
-    User.find()
-      .select('_id name email password age subscribed created')
-      .exec()
-      .then((users) => {
-        const response = {
-          count: users.length,
-          users: users.map((user) => ({
-            _id: user._id,
-            name: user.name,
-            email: user.email,
-            password: user.password,
-            age: user.age,
-            subscribed: user.subscribed,
-            created: user.created,
-            request: {
-              type: 'GET POST DELETE PUT',
-              _links: {
-                self: {
-                  href: `http://127.0.0.1:${port}/user/${user._id}`,
-                },
-                list: {
-                  href: `http://127.0.0.1:${port}/user`,
-                },
-                post: {
-                  href: `http://127.0.0.1:${port}/user`,
-                  data: {
-                    name: 'String',
-                    email: 'String',
-                    password: 'String',
-                  },
-                },
-                delete: {
-                  href: `http://127.0.0.1:${port}/user/${user._id}`,
-                },
-              },
-            },
-          })),
-        };
-
-        logger.info(response);
-        res.status(HttpStatus.OK).json(response);
-      })
-      .catch((err) => {
-        res.status(HttpStatus.INTERNAL_ERROR).json({ error: err });
-      });
-  })
-
-  // Get User by Id
-  .get('/:id', (req, res) => {
-    const { id } = req.params;
-    User.findById(id)
-      .select('_id name email password age subscribed created')
-      .exec()
-      .then((user) => {
-        if (user) {
-          res.status(HttpStatus.OK).json({
-            user,
-            request: {
-              type: 'GET PUT DELETE',
-              _links: {
-                self: {
-                  href: `http://127.0.0.1:${port}/user/${user._id}`,
-                },
-                put: {
-                  href: `http://127.0.0.1:${port}/user/${user._id}`,
-                },
-                delete: {
-                  href: `http://127.0.0.1:${port}/user/${user._id}`,
-                },
-              },
-            },
-          });
-        } else {
-          res
-            .status(HttpStatus.NOT_FOUND)
-            .json({ msg: 'No valid entry found for provided ID' });
-        }
-      })
-      .catch((err) => {
-        res.status(HttpStatus.INTERNAL_ERROR).json({ error: err });
-        logger.error(err);
-      });
-  })
-
   // Create User
   .post('/signup', (req, res) => {
     const { name, email, password } = req.body;
@@ -169,40 +83,32 @@ router
       });
   })
 
-  // Update User by Id
-  .put('/:id', (req, res) => {
-    const { id } = req.params;
-    const target = mongoose.Types.ObjectId(id);
-
-    const updateOps = {};
-    for (const ops of req.body) {
-      updateOps[ops.propName] = ops.value;
-    }
-
-    User.updateOne({ _id: target }, { $set: updateOps })
+  // Login for User
+  .post('/login', (req, res) => {
+    const { email, password } = req.body;
+    User.find({ email: email })
       .exec()
-      .then((result) => {
-        logger.info(result);
-        res.status(HttpStatus.OK).json({
-          message: 'User updated',
-          request: {
-            type: 'GET PUT DELETE',
-            _links: {
-              self: {
-                href: `http://127.0.0.1:${port}/user/${result._id}`,
-              },
-              put: {
-                href: `http://127.0.0.1:${port}/user/${result._id}`,
-              },
-              delete: {
-                href: `http://127.0.0.1:${port}/user/${result._id}`,
-              },
-            },
-          },
+      .then((user) => {
+        if (user.length < 1) {
+          return res
+            .status(HttpStatus.UNAUTHORIZED)
+            .json({ message: 'Auth failed' });
+        }
+        bcrypt.compare(password, user[0].password, (err, result) => {
+          if (err) {
+            return res
+              .status(HttpStatus.UNAUTHORIZED)
+              .json({ message: 'Auth failed' });
+          }
+          if (result) {
+            return res
+              .status(HttpStatus.OK)
+              .json({ message: 'Auth successful' });
+          }
+          res.status(HttpStatus.UNAUTHORIZED).json({ message: 'Auth failed' });
         });
       })
       .catch((err) => {
-        logger.error(err);
         res.status(HttpStatus.INTERNAL_ERROR).json({ error: err });
       });
   })
